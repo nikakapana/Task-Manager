80cbf3610a7649e1507acd768b039a2d098c907d
nikakapana <dev.nikakapana@gmail.com>
2023-02-15 22:28:18 +0400

    add settings for project

---
+++ src/app/pages/manager/project-setting/board-add-edit/board-add-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board-add-edit',
+  templateUrl: './board-add-edit.component.html',
+  styleUrls: ['./board-add-edit.component.scss']
+})
+export class BoardAddEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
