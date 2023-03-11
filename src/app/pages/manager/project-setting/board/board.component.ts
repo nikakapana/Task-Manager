fcbae39206413fc4fd319f96840555ed02cd7b71
mari chemia <marichemia10@gmail.com>
2023-03-11 17:11:16 +0400

    remove board page

--- src/app/pages/manager/project-setting/board/board.component.ts
+++ src/app/pages/manager/project-setting/board/board.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Board } from 'src/app/core/interfaces/board';
-import { BoardService } from 'src/app/core/services/board.service';
 
 @Component({
   selector: 'app-board',
@@ -11,34 +7,11 @@ import { BoardService } from 'src/app/core/services/board.service';
 })
 export class BoardComponent implements OnInit {
 
-  boards$: Observable<Board[]> = this.boardService.getBoards();
-  boardId!: number
-  board: Board = {} as Board
-
   constructor(
-    private boardService: BoardService,
-    private route: ActivatedRoute,
   ) {
 
   }
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      if (params['id']) {
-        this.boardId = +params['id']
-        this.getBoard();
-      }
-    })
   }
-
-  getBoard() {
-    this.boardService.getBoard(this.boardId).subscribe(board => {
-      console.log(board)
-      this.board = board
-    })
-  }
-
-
-
-
 
 }
--- src/app/pages/manager/project-setting/board/board.module.ts
+++ src/app/pages/manager/project-setting/board/board.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { BoardRoutingModule } from './board-routing.module';
-
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    BoardRoutingModule
+    CommonModule
   ]
 })
 export class BoardModule { }
